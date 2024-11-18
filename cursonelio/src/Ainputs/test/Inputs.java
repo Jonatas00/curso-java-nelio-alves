1b0b9cc7c6654a22bd38953e4e527d02a91fa254
Jonatas <djonatas31@gmail.com>
2024-11-18 10:39:06 -0300

    add inputs

---
+++ cursonelio/src/Ainputs/test/Inputs.java
@@ -0,0 +1,11 @@
+package Ainputs.test;
+
+import Ainputs.domain.CalculoArea;
+
+public class Inputs {
+  public static void main(String[] args) {
+    final CalculoArea calc = new CalculoArea();
+
+    calc.CalcularArea();
+  }
+}
